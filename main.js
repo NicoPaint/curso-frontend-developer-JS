090d1f4b9b96dbff5bad4d1408f8c6cc720ade6a
Nicolas Pinto <felipepinve@gmail.com>
2022-11-09 21:10:30 -0600

    se escribio una mejor funcion para desplegar el menu del usuario en el navbar

--- main.js
+++ main.js
@@ -6,10 +6,5 @@ menuEmail.addEventListener('click', toggleDesktopMenu);
 console.log(desktopMenu.classList);
 
 function toggleDesktopMenu(){
-    if(desktopMenu.getAttribute('class') == "desktop-menu inactive"){
-        desktopMenu.setAttribute("class", "desktop-menu");
-    }
-    else if(desktopMenu.getAttribute('class') == "desktop-menu"){
-        desktopMenu.setAttribute("class", "desktop-menu inactive");
-    }
+    desktopMenu.classList.toggle("inactive");
 }
