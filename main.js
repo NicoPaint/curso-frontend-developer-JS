e1511ff499241fa88839785b16507ee48c5d4a56
Nicolas Pinto <felipepinve@gmail.com>
2022-11-10 09:42:47 -0600

    se agrego el menu desplegable para mobile y se aplicaron los mediaquerys para este menu y el menu desktop

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const burgerMenu = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu")
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+burgerMenu.addEventListener("click", toggleMobileMenu);
 
-console.log(desktopMenu.classList);
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle("inactive");
+}
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle("inactive");
 }
